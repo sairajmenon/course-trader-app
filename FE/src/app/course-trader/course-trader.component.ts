b7b84add1543ed5db3c68b620327625f1abf2d09
LAPTOP-BV79KQF6\saira <sairajmenon@yahoo.co.in>
2021-04-09 21:58:37 -0700

    Front End initial code structure commit

---
+++ FE/src/app/course-trader/course-trader.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course-trader',
+  templateUrl: './course-trader.component.html',
+  styleUrls: ['./course-trader.component.css']
+})
+export class CourseTraderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ FE/src/app/forum-component/forum-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forum-component',
+  templateUrl: './forum-component.component.html',
+  styleUrls: ['./forum-component.component.css']
+})
+export class ForumComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
